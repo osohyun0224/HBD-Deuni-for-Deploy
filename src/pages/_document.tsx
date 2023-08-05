b1e8aa5caab3c01f58b95d706072008fcef11045
osohyun0224 <osohyun0224@naver.com>
2023-08-06 01:25:57 +0900

    Feat: Nanum+Pen+Script 폰트 적용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,11 @@
-import Document, { DocumentContext } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
-
+    
     try {
       ctx.renderPage = () =>
         originalRenderPage({
@@ -27,6 +27,22 @@ class MyDocument extends Document {
       sheet.seal()
     }
   }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&family=Nanum+Pen+Script&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
-export default MyDocument
+export default MyDocument;
