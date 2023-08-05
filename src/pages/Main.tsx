155fc9a40554edb40fa29e8a5b8cc3573f32ff93
osohyun0224 <osohyun0224@naver.com>
2023-08-06 02:01:10 +0900

    Fix: 배너 크기 올바르게 조정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,8 +3,11 @@ import styled from 'styled-components';
 
 const ImageContainer = styled.div`
   position: relative;
-  width: 100%;
+  width: 70%; 
+  height: 70vh;
   overflow: hidden;
+  margin: 0 auto; 
+  margin-top:20px;
 `;
 
 const SlideButton = styled.button`
@@ -22,6 +25,8 @@ const SlideButton = styled.button`
 const Image = styled.img`
   display: block;
   width: 100%;
+  height: 100%;
+  object-fit: cover; 
 `;
 
 const Main = () => {
