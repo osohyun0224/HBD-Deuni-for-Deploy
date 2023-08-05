2853f0e53743d5a6dd2848b85a5c0ae5888f9770
osohyun0224 <osohyun0224@naver.com>
2023-08-06 01:52:17 +0900

    Feat: Main화면 배너창 컴포넌트 구현

---
+++ src/pages/Main.tsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+const ImageContainer = styled.div`
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+`;
+
+const SlideButton = styled.button`
+  position: absolute;
+  top: 50%;
+  font-size: 2rem;
+  background: none;
+  border: none;
+  cursor: pointer;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Image = styled.img`
+  display: block;
+  width: 100%;
+`;
+
+const Main = () => {
+  const images = ['Banner1.jpg', 'Banner2.jpg', 'Banner3.jpg', 'Banner4.jpg', 'Banner5.jpg'];
+  const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrent((prevCurrent) => (prevCurrent + 1) % images.length);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [images.length]);
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? images.length - 1 : current - 1);
+  };
+
+  const nextSlide = () => {
+    setCurrent((current + 1) % images.length);
+  };
+
+  return (
+    <ImageContainer>
+      <SlideButton onClick={prevSlide} style={{ left: '10px' }}>
+        &#10094;
+      </SlideButton>
+      <Image src={`/images/${images[current]}`} alt="" />
+      <SlideButton onClick={nextSlide} style={{ right: '10px' }}>
+        &#10095;
+      </SlideButton>
+    </ImageContainer>
+  );
+};
+
+export default Main;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,6 +25,7 @@ const Title = styled.h1`
 `;
 
 const Button = styled.button`
+  font-family: "Nanum Pen Script", sans-serif;
   font-size: 20px;
   font-weight: 700;
   line-height: 22px;
